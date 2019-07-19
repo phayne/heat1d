4de010b8e41ad4c0e69fe71564a1ab2089d9b859
K.-Michael Aye <kmichael.aye@gmail.com>
2019-07-19 12:13:05 -0700

    Bump version: 0.1.4 -> 0.1.5

--- python/heat1d/__init__.py
+++ python/heat1d/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Paul O. Hayne"""
 __email__ = 'paul.hayne@lasp.colorado.edu'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 # pull everything from the original heat1d module
 # into the main namespace
