5713f7e2722b7e5fb66fbfe3c49d685e2c963418
Michael Aye <kmichael.aye@gmail.com>
2020-09-04 18:10:32 -0600

    insist on planets >= 0.4.6
    also black formatting

--- python/setup.py
+++ python/setup.py
@@ -5,48 +5,38 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 setup(
-    name='heat1d',
-    version='0.3.0',
+    name="heat1d",
+    version="0.3.0",
     description="Thermal model for planetary science applications",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     author="Paul O. Hayne",
-    author_email='paul.hayne@lasp.colorado.edu',
-    maintainer_email='kmichael.aye@gmail.com',
-    url='https://github.com/phayne/heat1d',
+    author_email="paul.hayne@lasp.colorado.edu",
+    maintainer_email="kmichael.aye@gmail.com",
+    url="https://github.com/phayne/heat1d",
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'heat1d=heat1d.cli:main'
-        ]
-    },
-    package_dir={'heat1d':
-                 'heat1d'},
+    entry_points={"console_scripts": ["heat1d=heat1d.cli:main"]},
+    package_dir={"heat1d": "heat1d"},
     include_package_data=True,
-    install_requires=[
-        'Click>=6.0',
-        'numpy',
-        'matplotlib',
-        'planets',
-    ],
+    install_requires=["Click>=6.0", "numpy", "matplotlib", "planets>=0.4.6",],
     license="MIT license",
     zip_safe=False,
-    keywords='heat1d',
+    keywords="heat1d",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
     ],
-    test_suite='tests',
-    tests_require=['pytest'],
-    setup_requires=['pytest_runner'],
+    test_suite="tests",
+    tests_require=["pytest"],
+    setup_requires=["pytest_runner"],
 )
